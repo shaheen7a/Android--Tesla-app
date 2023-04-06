9c5dc8ffd1f027bd044c4c18af7124f181135630
Shaheen7a <aousshaheen7@gmail.com>
2023-04-06 03:35:08 -0700

    Secondary Screen

--- components/MenuOption.jsx
+++ components/MenuOption.jsx
@@ -1,18 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 const MenuOption = ({ item }) => {
   return (
-    <View style={styles.optionRow}>
-      <MaterialCommunityIcons name={item.iconName} size={26} color="gray" />
-      <Text style={styles.optionText}>{item.name}</Text>
-      <MaterialIcons
-        name="keyboard-arrow-right"
-        size={24}
-        color="gray"
-        style={{ marginLeft: 'auto' }}
-      />
-    </View>
+    <Link href={item.href} asChild>
+      <Pressable style={styles.optionRow}>
+        <MaterialCommunityIcons name={item.iconName} size={26} color="gray" />
+        <Text style={styles.optionText}>{item.name}</Text>
+        <MaterialIcons
+          name="keyboard-arrow-right"
+          size={24}
+          color="gray"
+          style={{ marginLeft: 'auto' }}
+        />
+      </Pressable>
+    </Link>
   );
 };
 
